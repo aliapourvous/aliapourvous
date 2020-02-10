db9ce2eb3e2234b32b57fbc26b2f560f62c259a6
José dBruxelles <18559798+jdbruxelles@users.noreply.github.com>
2020-02-10 19:33:09 +0100

    Updates

---
+++ admin/main.js
@@ -0,0 +1,14 @@
+import CMS from "netlify-cms-app";
+import { fr } from "netlify-cms-locales";
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
+
+CMS.registerLocale("fr", fr);
