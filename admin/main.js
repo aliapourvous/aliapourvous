33541a3ff925039182aabfc2a028450b03151db2
José dBruxelles <18559798+jdbruxelles@users.noreply.github.com>
2020-07-10 16:08:31 +0200

    Add youtube widget and custom style for previews

--- admin/main.js
+++ admin/main.js
@@ -12,3 +12,24 @@ if (window.netlifyIdentity) {
 }
 
 // CMS.registerLocale("fr", fr);
+
+CMS.registerPreviewStyle("/admin/custom-preview.css");
+CMS.registerEditorComponent({
+  id: "youtube",
+  label: "Youtube",
+  fields: [{name: "id", label: "ID de la vidéo YouTube"}],
+  pattern: /^{{<\s?youtube (\S+)\s?>}}/,
+  fromBlock: function (match) {
+    return {
+      id: match[1]
+    };
+  },
+  toBlock: function (obj) {
+    return "{{< youtube " + obj.id + " >}}";
+  },
+  toPreview: function (obj) {
+    return (
+      '<img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video" style="max-width:100%"/>'
+    );
+  }
+});
